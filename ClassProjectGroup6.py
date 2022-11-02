3e91bf6b128990a6f0d1d7bce3ac6433b260315c
ACardenasSil <98552654+ACardenasSil@users.noreply.github.com>
2022-11-02 15:32:33 -0700

    Create ClassProjectGroup6.py

---
+++ ClassProjectGroup6.py
@@ -0,0 +1,10 @@
+#------------------------------
+# CMPS 3500 - Class Project: Basic Data Analysis Engine
+#
+# Jarl Ramos
+# Alonso Cardenas Sillas
+# Anh Vu
+# Shadi Abdul Razzak
+#---------------------------
+
+# Main file
