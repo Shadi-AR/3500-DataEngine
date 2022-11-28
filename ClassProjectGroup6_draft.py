3fdbebde0b55c12b0658365163287f2abfe27dc4
ACardenasSil <98552654+ACardenasSil@users.noreply.github.com>
2022-11-27 16:30:43 -0800

    Removed testing lines
    Removed lines after line:322 which were used for testing some functions.

--- ClassProjectGroup6_draft.py
+++ ClassProjectGroup6_draft.py
@@ -319,12 +319,3 @@ def top_5_airports(alist):
         new_dict.update(dict_buf)
         del alist[most_delayed_airport]
     return new_dict
-
-# Main file
-# import openFile
-# import mathlibrary as ml
-# myArray = [ 1, 4, 2, 8, 3, 4, 3, 5, 6, 6, 7, 2, 7, 9, 1, 0] # 16 random items
-# c = 0
-# c = ml.mode(myArray)
-# print(c)
-
