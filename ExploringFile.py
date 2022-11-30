051f64666dc0a1d037c6f91a3296205634f2314b
AnhVu8499 <98485978+AnhVu8499@users.noreply.github.com>
2022-11-30 10:33:32 -0800

    updated to handle some errors

--- ExploringFile.py
+++ ExploringFile.py
@@ -3,60 +3,66 @@
 
 import pandas as pd
 
-fname = '/home/fac/walter/public_html/courses/cs3500/2022_fall/proj/data/2019_Airline_Delays_Dataset_train_Part1.csv' 
-
+#fname = '/home/fac/walter/public_html/courses/cs3500/2022_fall/proj/data/2019_Airline_Delays_Dataset_train_Part2.csv' 
+fname = input("Enter the file name: ")
 def readByPandas(fname):
-    df = pd.read_csv(fname)
-    count = 0
-    #Printing Column Names
-    for colName in range (26):
-        colName = df.iloc[0:0, colName].name
-        print (colName) 
+    try:
+        df = pd.read_csv(fname)
+        count = 0
+        #Printing Column Names
+        for colName in range (26):
+            colName = df.iloc[0:0, colName].name
+            print (colName) 
+    except FileNotFoundError:
+        print("File was not found")
+    try:    
+        #print unique values 
+        colUnique = input("Unique values of columns :")
+        uniqueVal = df[colUnique].unique()
+        print ("Unique values of chosen values are:", end = " ")
+        print (uniqueVal)
+        #Counting unique values
+        for i in uniqueVal:
+            if i != (i+1):
+                count += 1
+        print("Unique values of this columns:", end = " ")
+        print(count)
 
-    #print unique values of chosen columns
-    colUnique = input("Unique values of columns :")
-    uniqueVal = df[colUnique].unique()
-    print ("Unique values of chosen values are:", end = " ")
-    print (uniqueVal)
-    
-    #Counting unique values
-    for i in uniqueVal:
-        if i != (i+1):
-            count += 1
-    print("Unique values of this columns:", end = " ")
-    print(count)
+        #Drop a column
+        dropInput = input("Name of columns to drop: ")
+        drop = df.drop(columns=(dropInput))
+        print(drop)
 
-    #Drop a column
-    dropInput = input("Name of columns to drop: ")
-    drop = df.drop(columns=(dropInput))
-    print(drop)
+        #Search input values
+        searchColInput = input("Columns to search for: ")
+        searchCol = df[searchColInput]
+        if searchColInput != dropInput:
+            print(searchCol)
+            searchInput = input("Values to search for: " )
+            counting = 0
 
-    #Search input values
-    searchColInput = input("Columns to search for: ")
-    searchCol = df[searchColInput]
-    print(searchCol)
-    searchInput = input("Values to search for: " )
-    counting = 0
-    """
+            """
     for i in searchCol:
         if str(i) in searchInput:
             counting += 1
             print(df.loc[i])
     print(counting)
-"""
-    totalRow = 0
-    for i in searchCol:
-        totalRow+=1
-        if str(i) in searchInput:
-            counting += 1
-            print("Elements found in row: ", end= " ")
-            print(totalRow)
-    print("Elements were found in ", end= "" )
-    print(counting, end =" ")
-    print("rows")
+            """
+            totalRow = 0
+            for i in searchCol:
+                totalRow+=1
+                if str(i) in searchInput:
+                    counting += 1
+                    #print("Elements found in row: ", end= " ")
+                    #print(totalRow)
+            print("Elements were found in ", end= "" )
+            print(counting, end =" ")
+            print("rows")
+        else:    
+            print("Column was not found")
+    except KeyError:
+        print("Input does not exist")
 
-#call function
-readByPandas(fname)
 
 
 
